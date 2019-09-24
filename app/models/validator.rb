7687fcee8b32ebe8cb415d817ef0685928599376
Scott Silverman <scottms724@gmail.com>
2019-09-24 14:04:10 +0000

    Automatically backed up by Learn

--- app/models/validator.rb
+++ app/models/validator.rb
@@ -2,7 +2,7 @@ class Validator < ActiveModel::Validator
   def validate(record)
     if record.title != nil
       unless record.title.match?(/(You|Won't Believe|Secret|Top \d|Guess)/)
-        record.errors[:title] << 'Need a clickbait title please!'
+        record.errors[:title] << 'Please add a clickbait title!'
       end
     end
   end
